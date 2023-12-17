523c3db971fbe3b5d9998390508d38d4cc34bb1d
TWilliam <noemail>
2023-12-17 21:35:47 +0300

    replaces class Stat with interface

--- Assets/Runtime/Scripts/Leveling/Interfaces/IStatContainer.cs
+++ Assets/Runtime/Scripts/Leveling/Interfaces/IStatContainer.cs
@@ -6,8 +6,8 @@ namespace Entities.Stats
     public interface IStatContainer
     {
         EffectStorage effectStorage { get; init; }
-        void InitStat<T>(Stat<T> stat, int baseValue) where T: IStatStorage;
-        void AddObserver<T, U>(IObserver<T>  observer, Stat<U> stat) where U : T;
+        void InitStat<T>(IStat<T> stat, int baseValue) where T: IStatStorage;
+        void AddObserver<T, U>(IObserver<T>  observer, IStat<U> stat) where U : T;
         Dictionary<StaticStat, StaticStatStorage> staticStatStorage { get; }
         Dictionary<CappedStat, CappedStatStorage> cappedStatStorage { get; }
     }
