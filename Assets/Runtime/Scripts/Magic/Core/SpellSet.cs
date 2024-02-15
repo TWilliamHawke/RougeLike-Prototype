a35fb3d4511695da607cc73aa30feb87eb100815
TWilliam <noemail>
2024-02-15 11:59:23 +0300

    added databaseSet

---
+++ Assets/Runtime/Scripts/Magic/Core/SpellSet.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Magic
+{
+	[CreateAssetMenu(fileName = "SpellSet", menuName = "EffectHandlers/SpellSet")]
+    public class SpellSet : DatabaseSet<Spell>
+    {
+
+    }
+}
