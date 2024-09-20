48a2b8ba61e5f46bffd448558d4398a03c33c5fc
TWilliam <noemail>
2024-09-20 20:20:41 +0300

    updated modal window

--- Assets/Runtime/Scripts/Core/UI/ModalWindow/ModalWindowController.cs
+++ Assets/Runtime/Scripts/Core/UI/ModalWindow/ModalWindowController.cs
@@ -6,30 +6,61 @@ namespace Core.UI
 {
     public class ModalWindowController : MonoBehaviour
     {
-        [SerializeField] ModalWindow _modalWindow;
+        [SerializeField] UIScreen _modalWindow;
+        [SerializeField] ModalWindowComponents _components;
 
-        [SerializeField] Injector _selfInjector;
-
-        private void Awake()
-        {
-			_selfInjector.SetDependency(this);
-        }
+        IContextAction _action;
 
         public void OpenWindow(ModalWindowData windowData)
         {
-            _modalWindow.Open(windowData);
+            _action = windowData.action;
+            _components.ResetAll();
+            _components.ChangeTitle(windowData.title);
+            TryShowText(windowData);
+            TryShowImage(windowData);
+            TryShowResources(windowData);
+            TryChangeConfirmButton(windowData);
+            _modalWindow.Open();
         }
 
-        // public bool TryCreateActionWrapper(ref IContextAction action)
-        // {
-        //     var dataCreator = action as IHaveModalWindowData;
-        //     if (dataCreator is null) return false;
-            
-        //     var data = new ModalWindowData();
-        //     if (!dataCreator.TryFillModalWindowData(ref data)) return false;
-        //     action = new OpenModalWindow(this, data);
-        //     return true;
-        // }
+        public void TryShowText(ModalWindowData windowData)
+        {
+            if (windowData.mainText == "") return;
+            _components.ShowText(windowData.mainText);
+        }
+
+        public void TryShowImage(ModalWindowData windowData)
+        {
+            if (windowData.mainImage == null) return;
+            _components.ShowImage(windowData.mainImage);
+        }
+
+        public void TryShowResources(ModalWindowData windowData)
+        {
+            if (windowData.resourcesData == null) return;
+            _components.ShowResources(windowData.resourcesData);
+        }
+
+        public void TryChangeConfirmButton(ModalWindowData windowData)
+        {
+            if (windowData.action == null) return;
+            _components.ChangeConfirmButtonText(windowData.action.actionTitle);
+            _components.ShowCloseButton();
+        }
+
+        //Used in Unity Editor
+        public void ConfirmButtonHandler()
+        {
+            _modalWindow.Close();
+            _action?.DoAction();
+            _action = null;
+        }
+
+        //Used in Unity Editor
+        public void CloseButtonHandler()
+        {
+            _modalWindow?.Close();
+        }
     }
 }
 
